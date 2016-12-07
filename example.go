750a81e30ce264bfd905d9cd376c0abb207fe04e
Zach Retterath <ztretterath@gmail.com>
2016-12-07 13:56:04 -0500

    Create example of bools vars ints and floats

--- example.go
+++ example.go
@@ -3,5 +3,30 @@ package main
 import "fmt"
 
 func main(){
-  fmt.Println("hello world")
+
+  // fmt.Println("hello world") //like consoloe.log or puts
+
+  // var age int = 40
+  //
+  // var num float64 = 1.6184
+  //
+  // // randNum := 1
+  //
+  // fmt.Println(age, num)
+  //
+  // var numOne = 1.000
+  // var num99 = .999
+  //
+  // fmt.Println(numOne - num99) //floats not always accurate
+
+  const pi float64 = 3.14159265
+  //
+  // var myName string = "Zach R"
+  //
+  // fmt.Println(len(myName))
+
+  // var isOver20 bool = true
+
+  fmt.Printf("%.3f \n", pi) //specify float length
+
 }
